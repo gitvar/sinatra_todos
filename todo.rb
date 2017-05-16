f637c6566e0908358b85bf6601b20c1823c65d60
gitvar <benvanderm@gmail.com>
2017-05-16 22:54:46 +0200

    Creating a new todo list. Assignment 6.

--- todo.rb
+++ todo.rb
@@ -4,7 +4,7 @@ require "tilt/erubis"
 
 configure do
   enable :sessions
-  set :session_secret, 'secret'
+  set :session_secret, 'secret' # This is a session variable.
 end
 
 before do
@@ -15,19 +15,47 @@ get "/" do
   redirect "/lists"
 end
 
+# The Routes built so far are:
+# GET  /lists      -> view all lists
+# GET  /lists/new  -> new list form
+# POST /lists/new  -> create a new list
+
+# The above names are 'resource based' names.
+# Meaning that the name of the things being modified is named in the URL.
+# Everything so far is modifying a "lists" object so there is a 'lists' element
+# at the very beginning of the URL. The name indicates what a URL IS rahter
+# than what the URL will DO.
+
+# Lets extrapolate out to what we know what this app is to ba able to do:
+# a. We need to VIEW a list and also view all of its todo items. This will be
+# a get as we are fetching data from a server. So, get a single list:
+
+# GET  /lists/1   -> view a single list (the 1 is an id which uniquely
+#                    identifes the list we want to see).
+
+# What about users?
+
+# GET  /users     -> view all the users
+# GET  /users/1   -> view one specific user with unique id == 1
+
+# See the pattern? fetch the "resourse type" (list or user in this case)
+# and if need be supply an unique identifier to fetch a single resource...
+
+# View all the lists (list of lists)
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
-
+  # The above line is the same as:
   # erb("You have no lists.", { layout: :layout })
 end
 
+# Render a new list form
 get "/lists/new" do
-  session[:lists] << { name: "New List", todos: [] }
-  redirect "/lists"
+  erb :new_list
 end
 
-# [
-#   { name: "Lunch Groceries", todos: ["Bread", "Butter", "Cheese"] },
-#   { name: "Dinner Groceries", todos: ["Steak", "Potatoes"] }
-# ]
+# Create a new list
+post "/lists" do
+  session[:lists] << { name: params[:list_name], todos: [] }
+  redirect "/lists"
+end
