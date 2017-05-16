8e538afc73b60730b1ac68b7a46d218b6848829e
gitvar <benvanderm@gmail.com>
2017-05-16 16:07:25 +0200

    Before the "new list" form.

--- todo.rb
+++ todo.rb
@@ -2,16 +2,32 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    { name: "Lunch Groceries", todos: ["Bread", "Butter", "Cheese"] },
-    { name: "Dinner Groceries", todos: ["Steak", "Potatoes"] }
-  ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
 
   # erb("You have no lists.", { layout: :layout })
 end
+
+get "/lists/new" do
+  session[:lists] << { name: "New List", todos: [] }
+  redirect "/lists"
+end
+
+# [
+#   { name: "Lunch Groceries", todos: ["Bread", "Butter", "Cheese"] },
+#   { name: "Dinner Groceries", todos: ["Steak", "Potatoes"] }
+# ]
