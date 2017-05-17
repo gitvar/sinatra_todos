6827b61787f9ef62aaa59eb046586617c6af8076
gitvar <benvanderm@gmail.com>
2017-05-17 16:15:00 +0200

    First iteration for the "error_for_list_name" method.

--- todo.rb
+++ todo.rb
@@ -29,14 +29,22 @@ get "/lists/new" do
   erb :new_list
 end
 
+# Return error message if the name is INVALID, else return nil.
+def error_for_list_name(name)
+  error = nil
+  if !(1..100).cover?(name.size)
+    error = "The new list name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    error = "The new list name must be unique."
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip # strip leading & trailing spaces
-  if !(1..100).cover?(list_name.size)
-    session[:error] = "The new list name must be between 1 and 100 characters."
-    erb :new_list
-  elsif session[:lists].any? { |list| list[:name] == list_name }
-    session[:error] = "The new list name must be unique."
+
+  if error = error_for_list_name(list_name)
+    session[:error] = error
     erb :new_list
   else
     session[:lists] << { name: list_name, todos: [] }
